9f7489c9462e367550e93ef31cd599d35da52a0d
george <rpubaddr0@gmail.com>
2013-10-20 21:36:54 -0600

    Corrected testreader.

--- testread.py
+++ testread.py
@@ -1,11 +1,28 @@
 import pymongo
+from bson.son import SON
 
 conn = pymongo.Connection('127.0.0.1')
 db = conn['geodigger_test']
 coll = db['data_test']
 
-coll.create_index([('loc', pymongo.GEOSPHERE)])
+#coll.create_index([('loc', pymongo.GEOSPHERE)])
+coll.ensure_index([(u'loc', pymongo.GEOSPHERE)])
 
-print coll.find({
+c = {"loc":
+            SON([
+                ("$near", SON([
+                    ("$geometry", SON([
+                        ("type", "Point"),
+                        ("coordinates", [-105.2, 39.7])
+                        ])
+                    ),
+                    ("$maxDistance", 10000)
+                    ])
+                )
+            ])
+}
 
-        })
+a = coll.find(c)
+
+for b in a:
+    print b

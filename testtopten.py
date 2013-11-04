5a6e52f49bfb0eabcafebbe932335ea9f4ca1f77
george <rpubaddr0@gmail.com>
2013-11-04 13:47:17 -0700

    Removed commented code from testTopTen.

--- testtopten.py
+++ testtopten.py
@@ -5,8 +5,6 @@ conn = pymongo.Connection('127.0.0.1')
 db = conn['geodigger_test']
 coll = db['data_test']
 
-#coll.ensure_index([(u'userID', pymongo.GEOSPHERE)])
-
 a = coll.aggregate([
         {"$group": {
             "_id": "$userID",
@@ -18,22 +16,3 @@ a = coll.aggregate([
 
 for b in a['result']:
     print "%s,%s" % (b['_id'], b['number'])
-
-#c = {"loc":
-#            SON([
-#                ("$near", SON([
-#                    ("$geometry", SON([
-#                        ("type", "Point"),
-#                        ("coordinates", [-105.2, 39.7])
-#                        ])
-#                    ),
-#                    ("$maxDistance", 10000)
-#                    ])
-#                )
-#            ])
-#}
-
-#a = coll.find(c)
-
-#for b in a:
-#    print b

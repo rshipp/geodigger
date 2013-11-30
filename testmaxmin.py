e3608e51fe5bc22fc48501b72bd878003e3eaffe
george <rpubaddr0@gmail.com>
2013-11-29 18:46:24 -0700

    Added new aggregate test for max/min date.

---
+++ testmaxmin.py
@@ -0,0 +1,26 @@
+import pymongo
+from bson.son import SON
+
+conn = pymongo.Connection('127.0.0.1')
+db = conn['geodigger']
+coll = db['data']
+
+a = coll.aggregate([
+        {"$group": {
+            "_id": 0,
+            "minDate": {"$min": "$time"}
+        }},
+])
+
+for b in a['result']:
+    print "Min date: ", b['minDate'])
+
+c = coll.aggregate([
+        {"$group": {
+            "_id": 0,
+            "maxDate": {"$max": "$time"}
+        }},
+])
+
+for d in c['result']:
+    print "Max date: ", d['maxDate']

9bb29f8eab08c412769fa925deeb9ef796a73bc1
george <rpubaddr0@gmail.com>
2013-11-30 19:06:58 -0700

    onepage generation fixes.

--- onepage.sh
+++ onepage.sh
@@ -2,12 +2,13 @@
 # onepage.sh
 # Combine all documents into one large file.
 
-pages=( index.md twitter.md tweepy.md mongodb.md geojson.md pyramid.md
-        geodigger.md twitterdigger.md postprocessor.md ui.md )
+pages=( onepage_disclaimer.txt index.md twitter.md tweepy.md mongodb.md
+        geojson.md pyramid.md geodigger.md twitterdigger.md
+        postprocessor.md ui.md )
 onepage=onepage.md
 
 rm $onepage
 for page in ${pages[@]}; do
-    sed 's/\[\([a-z]*\).md\]/[#\1]/g' $page >> $onepage
+    sed 's/(\([a-z]*\).md)/(#\1)/g; s/([a-z]*.md\(#[a-z]*\))/\1/g' $page >> $onepage
     echo >> $onepage
 done

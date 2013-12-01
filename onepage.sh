cf5eca3134d89e83c80999c002c2157a296b1179
george <rpubaddr0@gmail.com>
2013-11-30 18:58:10 -0700

    Testing new unified document.

---
+++ onepage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# onepage.sh
+# Combine all documents into one large file.
+
+pages=( index.md twitter.md tweepy.md mongodb.md geojson.md pyramid.md
+        geodigger.md twitterdigger.md postprocessor.md ui.md )
+onepage=onepage.md
+
+rm $onepage
+for page in ${pages[@]}; do
+    sed 's/\[\([a-z]*\).md\]/[#\1]/g' $page >> $onepage
+    echo >> $onepage
+done
